f29022797ad521272fefbb39ad2ec4d476496b3e
umoazzam <usmanmoazzam@hotmail.com>
2025-10-21 21:38:38 -0700

    fixed checkstyle alerts in model and controller folders

--- src/main/java/com/columbia/coms4156/citationservice/model/Article.java
+++ src/main/java/com/columbia/coms4156/citationservice/model/Article.java
@@ -1,111 +1,212 @@
 package com.columbia.coms4156.citationservice.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "articles")
 public class Article extends Source {
 
-  // Instance Vairables
+    /**
+     * The journal where the article was published.
+     */
     @Column
     private String journal;
 
+    /**
+     * The volume number of the journal.
+     */
     @Column
     private String volume;
 
+    /**
+     * The issue number of the journal.
+     */
     @Column
     private String issue;
 
+    /**
+     * The page numbers of the article.
+     */
     @Column
     private String pages;
 
+    /**
+     * The DOI (Digital Object Identifier) of the article.
+     */
     @Column
     private String doi;
 
+    /**
+     * The URL of the article.
+     */
     @Column
     private String url;
 
+    /**
+     * The publication year of the article.
+     */
     @Column
     private Integer publicationYear;
 
-    // Constructors
-    public Article() { super(); }
+    /**
+     * Default constructor for Article.
+     */
+    public Article() {
+        super();
+    }
 
+    /**
+     * Constructor for Article with title and author.
+     *
+     * @param title the title of the article
+     * @param author the author of the article
+     */
     public Article(String title, String author) {
         super(title, author);
     }
 
-    // Methods
+    /**
+     * Gets the journal name.
+     *
+     * @return the journal name
+     */
     public String getJournal() {
         return journal;
     }
 
-    public void setJournal(String journal) {
-        this.journal = journal;
+    /**
+     * Sets the journal name.
+     *
+     * @param journalParam the journal name to set
+     */
+    public void setJournal(String journalParam) {
+        this.journal = journalParam;
     }
 
+    /**
+     * Gets the volume number.
+     *
+     * @return the volume number
+     */
     public String getVolume() {
         return volume;
     }
 
-    public void setVolume(String volume) {
-        this.volume = volume;
+    /**
+     * Sets the volume number.
+     *
+     * @param volumeParam the volume number to set
+     */
+    public void setVolume(String volumeParam) {
+        this.volume = volumeParam;
     }
 
+    /**
+     * Gets the issue number.
+     *
+     * @return the issue number
+     */
     public String getIssue() {
         return issue;
     }
 
-    public void setIssue(String issue) {
-        this.issue = issue;
+    /**
+     * Sets the issue number.
+     *
+     * @param issueParam the issue number to set
+     */
+    public void setIssue(String issueParam) {
+        this.issue = issueParam;
     }
 
+    /**
+     * Gets the page numbers.
+     *
+     * @return the page numbers
+     */
     public String getPages() {
         return pages;
     }
 
-    public void setPages(String pages) {
-        this.pages = pages;
+    /**
+     * Sets the page numbers.
+     *
+     * @param pagesParam the page numbers to set
+     */
+    public void setPages(String pagesParam) {
+        this.pages = pagesParam;
     }
 
+    /**
+     * Gets the DOI.
+     *
+     * @return the DOI
+     */
     public String getDoi() {
         return doi;
     }
 
-    public void setDoi(String doi) {
-        this.doi = doi;
+    /**
+     * Sets the DOI.
+     *
+     * @param doiParam the DOI to set
+     */
+    public void setDoi(String doiParam) {
+        this.doi = doiParam;
     }
 
+    /**
+     * Gets the URL.
+     *
+     * @return the URL
+     */
     public String getUrl() {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    /**
+     * Sets the URL.
+     *
+     * @param urlParam the URL to set
+     */
+    public void setUrl(String urlParam) {
+        this.url = urlParam;
     }
 
+    /**
+     * Gets the publication year.
+     *
+     * @return the publication year
+     */
     public Integer getPublicationYear() {
         return publicationYear;
     }
 
-    public void setPublicationYear(Integer publicationYear) {
-        this.publicationYear = publicationYear;
+    /**
+     * Sets the publication year.
+     *
+     * @param publicationYearParam the publication year to set
+     */
+    public void setPublicationYear(Integer publicationYearParam) {
+        this.publicationYear = publicationYearParam;
     }
 
     @Override
     public String toString() {
-        return "Article{" +
-                "id=" + getId() +
-                ", title='" + getTitle() + '\'' +
-                ", author='" + getAuthor() + '\'' +
-                ", journal='" + journal + '\'' +
-                ", volume='" + volume + '\'' +
-                ", issue='" + issue + '\'' +
-                ", pages='" + pages + '\'' +
-                ", doi='" + doi + '\'' +
-                ", url='" + url + '\'' +
-                ", publicationYear=" + publicationYear +
-                '}';
+        return "Article{"
+                + "id=" + getId()
+                + ", title='" + getTitle() + '\''
+                + ", author='" + getAuthor() + '\''
+                + ", journal='" + journal + '\''
+                + ", volume='" + volume + '\''
+                + ", issue='" + issue + '\''
+                + ", pages='" + pages + '\''
+                + ", doi='" + doi + '\''
+                + ", url='" + url + '\''
+                + ", publicationYear=" + publicationYear
+                + '}';
     }
 }
 
--- src/main/java/com/columbia/coms4156/citationservice/model/Video.java
+++ src/main/java/com/columbia/coms4156/citationservice/model/Video.java
@@ -1,87 +1,162 @@
 package com.columbia.coms4156.citationservice.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "videos")
 public class Video extends Source {
 
-    // Instance Variables
+    /**
+     * The director of the video.
+     */
     @Column
     private String director;
 
+    /**
+     * The duration of the video in seconds.
+     */
     @Column
     private Integer durationSeconds;
 
+    /**
+     * The platform where the video is hosted.
+     */
     @Column
     private String platform;
 
+    /**
+     * The URL of the video.
+     */
     @Column
     private String url;
 
+    /**
+     * The release year of the video.
+     */
     @Column
     private Integer releaseYear;
 
-    // Constructors
-    public Video() { super(); }
+    /**
+     * Default constructor for Video.
+     */
+    public Video() {
+        super();
+    }
 
+    /**
+     * Constructor for Video with title and author.
+     *
+     * @param title the title of the video
+     * @param author the author of the video
+     */
     public Video(String title, String author) {
         super(title, author);
     }
 
-    // Methods
+    /**
+     * Gets the director of the video.
+     *
+     * @return the director
+     */
     public String getDirector() {
         return director;
     }
 
-    public void setDirector(String director) {
-        this.director = director;
+    /**
+     * Sets the director of the video.
+     *
+     * @param directorParam the director to set
+     */
+    public void setDirector(String directorParam) {
+        this.director = directorParam;
     }
 
+    /**
+     * Gets the duration in seconds.
+     *
+     * @return the duration in seconds
+     */
     public Integer getDurationSeconds() {
         return durationSeconds;
     }
 
-    public void setDurationSeconds(Integer durationSeconds) {
-        this.durationSeconds = durationSeconds;
+    /**
+     * Sets the duration in seconds.
+     *
+     * @param durationSecondsParam the duration in seconds to set
+     */
+    public void setDurationSeconds(Integer durationSecondsParam) {
+        this.durationSeconds = durationSecondsParam;
     }
 
+    /**
+     * Gets the platform where the video is hosted.
+     *
+     * @return the platform
+     */
     public String getPlatform() {
         return platform;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    /**
+     * Sets the platform where the video is hosted.
+     *
+     * @param platformParam the platform to set
+     */
+    public void setPlatform(String platformParam) {
+        this.platform = platformParam;
     }
 
+    /**
+     * Gets the URL of the video.
+     *
+     * @return the URL
+     */
     public String getUrl() {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    /**
+     * Sets the URL of the video.
+     *
+     * @param urlParam the URL to set
+     */
+    public void setUrl(String urlParam) {
+        this.url = urlParam;
     }
 
+    /**
+     * Gets the release year of the video.
+     *
+     * @return the release year
+     */
     public Integer getReleaseYear() {
         return releaseYear;
     }
 
-    public void setReleaseYear(Integer releaseYear) {
-        this.releaseYear = releaseYear;
+    /**
+     * Sets the release year of the video.
+     *
+     * @param releaseYearParam the release year to set
+     */
+    public void setReleaseYear(Integer releaseYearParam) {
+        this.releaseYear = releaseYearParam;
     }
 
     @Override
     public String toString() {
-        return "Video{" +
-                "id=" + getId() +
-                ", title='" + getTitle() + '\'' +
-                ", author='" + getAuthor() + '\'' +
-                ", director='" + director + '\'' +
-                ", durationSeconds=" + durationSeconds +
-                ", platform='" + platform + '\'' +
-                ", url='" + url + '\'' +
-                ", releaseYear=" + releaseYear +
-                '}';
+        return "Video{"
+                + "id=" + getId()
+                + ", title='" + getTitle() + '\''
+                + ", author='" + getAuthor() + '\''
+                + ", director='" + director + '\''
+                + ", durationSeconds=" + durationSeconds
+                + ", platform='" + platform + '\''
+                + ", url='" + url + '\''
+                + ", releaseYear=" + releaseYear
+                + '}';
     }
 }
 
